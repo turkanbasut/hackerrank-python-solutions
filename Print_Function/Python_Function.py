1a36782a1ab2c4bf47804e024a08bf0ca79f767d
turkanbasut <89381428+turkanbasut@users.noreply.github.com>
2022-08-06 15:21:29 +0300

    Update Python_Function.py

--- Print_Function/Python_Function.py
+++ Print_Function/Python_Function.py
@@ -1,4 +1,4 @@
-# The included code stub will read an integer, , from STDIN.
+# The included code stub will read an integer, n, from STDIN.
 #
 # Without using any string methods, try to print the following:
 # 123...n
