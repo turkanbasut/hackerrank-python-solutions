74d4c80db552f6cf4cf03708e9642cc36ad6d5ba
turkanbasut <89381428+turkanbasut@users.noreply.github.com>
2022-08-06 16:06:44 +0300

    Create What's_Your_Name?.py

---
+++ What's_Your_Name?/What's_Your_Name?.py
@@ -0,0 +1,29 @@
+# Problem
+# You are given the firstname and lastname of a person on two different lines. Your task is to read them and print the following:
+#
+# Hello firstname lastname! You just delved into python.
+# Input Format
+# The first line contains the first name, and the second line contains the last name.
+#
+# Constraints
+# The length of the first and last name ≤ 10.
+#
+# Output Format
+# Print the output as mentioned above.
+#
+# Sample Input 0
+#
+# Ross
+# Taylor
+# Sample Output 0
+#
+# Hello Ross Taylor! You just delved into python.
+
+
+def print_full_name(a, b):
+    print("Hello {} {}! You just delved into python.".format(a,b))
+
+if __name__ == '__main__':
+    first_name = input()
+    last_name = input()
+    print_full_name(first_name, last_name)
